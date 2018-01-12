1ce89814b949484c63e9981cbe343f0f25b91c11
Andrei <andreiazotov@gmail.com>
2018-01-12 21:52:39 +0300

    Initialize project

---
+++ [Building_a_Graph]/Assets/Scripts/Graph.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        Instantiate(this.pointPrefab);
+    }
+}
