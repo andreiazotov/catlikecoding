d4ec2dd693293c89e89808229cd9adad8e64db7f
Andrei <andreiazotov@gmail.com>
2018-01-15 22:30:43 +0300

    Add new Sine function & function switch

--- [Mathematical_Surfaces]/Assets/Scripts/Graph.cs
+++ [Mathematical_Surfaces]/Assets/Scripts/Graph.cs
@@ -7,10 +7,27 @@ public class Graph : MonoBehaviour
     [Range(10, 100)]
     public int resolution = 10;
 
+    /*
+     * This makes it possible to control the function via the graph's
+     * inspector, also while we're in play mode.
+    */
+    [Range(0, 1)]
+    public int function;
+
     private Transform[] _points;
 
     private void Awake()
     {
+        /* It is possible change the code and the function will change
+         * along with it, even while the Unity editor
+         * is in play mode. Execution will be paused, the current game
+         * state saved, then the scripts are compiled again, and finally
+         * the game state is reloaded and play resumes. Not everything
+         * survives a recompile while in play mode, but graph does.
+         * It will switch to animating the new function, without being
+         * aware that something changed.
+        */
+
         float step = 2.0f / this.resolution;
         var scale = Vector3.one * step;
         Vector3 position;
@@ -32,12 +49,33 @@ public class Graph : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float t = Time.time;
         for (int i = 0; i < this._points.Length; i++)
         {
             var point = this._points[i];
             var position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            if (function == 0)
+            {
+                position.y = this.SineFunction(position.x, t);
+            }
+            else
+            {
+                position.y = this.MultiSineFunction(position.x, t);
+            }
             point.localPosition = position;
         }
+    }
+
+    private float SineFunction(float x, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    private float MultiSineFunction(float x, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2.0f * Mathf.PI * (x + 2.0f * t)) / 2.0f;
+        y *= 2.0f / 3.0f;
+        return y;
     }
 }
