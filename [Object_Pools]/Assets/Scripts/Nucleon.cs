0aea1e7cfad25207194a130c78c059216939e9d3
Andrei <andreiazotov@gmail.com>
2018-01-29 21:37:10 +0300

    [O.POOLS] Initialize project

---
+++ [Object_Pools]/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    public float attractionForce;
+
+    private Rigidbody _body;
+
+    private void Awake()
+    {
+        this._body = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        this._body.AddForce(this.transform.localPosition * -this.attractionForce);
+    }
+}
