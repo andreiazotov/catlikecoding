a90b79ca925c32bcdae2bb3116b8f4e4afc03ce3
Andrei <andreiazotov@gmail.com>
2018-02-05 22:38:30 +0300

    [CURVES] Add complex Bezier curve with directions

--- [Curves_and_Splines]/Assets/Scripts/BezierCurve.cs
+++ [Curves_and_Splines]/Assets/Scripts/BezierCurve.cs
@@ -10,7 +10,23 @@ public class BezierCurve : MonoBehaviour
         {
             new Vector3(1.0f, 0.0f, 0.0f),
             new Vector3(2.0f, 0.0f, 0.0f),
-            new Vector3(3.0f, 0.0f, 0.0f)
+            new Vector3(3.0f, 0.0f, 0.0f),
+            new Vector3(4.0f, 0.0f, 0.0f),
         };
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        return this.transform.TransformPoint(Bezier.GetPoint(this.points[0], this.points[1], this.points[2], this.points[3], t));
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        return this.transform.TransformPoint(Bezier.GetFirstDerivative(this.points[0], this.points[1], this.points[2], this.points[3], t)) - this.transform.position;
+    }
+
+    public Vector3 GetDirection(float t)
+    {
+        return this.GetVelocity(t).normalized;
     }
 }
