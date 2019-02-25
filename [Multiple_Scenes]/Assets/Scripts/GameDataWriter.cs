3f687c0651f4199b119f8555f4968a5d22d20621
Andrei <andreiazotov@gmail.com>
2019-02-25 23:24:53 +0300

    [MULTI.SCENES] Complete Tutorial

---
+++ [Multiple_Scenes]/Assets/Scripts/GameDataWriter.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class GameDataWriter
+{
+    private BinaryWriter _writer;
+
+    public GameDataWriter(BinaryWriter writer) {
+        _writer = writer;
+    }
+
+    public void Write(float value) {
+        _writer.Write(value);
+    }
+
+    public void Write(int value) {
+        _writer.Write(value);
+    }
+
+    public void Write(Quaternion value) {
+        _writer.Write(value.x);
+        _writer.Write(value.y);
+        _writer.Write(value.z);
+        _writer.Write(value.w);
+    }
+
+    public void Write(Vector3 value) {
+        _writer.Write(value.x);
+        _writer.Write(value.y);
+        _writer.Write(value.z);
+    }
+
+    public void Write(Color color) {
+        _writer.Write(color.r);
+        _writer.Write(color.g);
+        _writer.Write(color.b);
+        _writer.Write(color.a);
+    }
+}
