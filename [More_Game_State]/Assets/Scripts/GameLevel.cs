d0da3a6eff7a8ce97d85d5be2f7416d2ca48d011
Andrei <andreiazotov@gmail.com>
2019-03-01 20:48:05 +0300

    [MORE GAME STATE] Complete Tutorial

---
+++ [More_Game_State]/Assets/Scripts/GameLevel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameLevel : PersistableObject
+{
+    [SerializeField]
+    private SpawnZone _spawnZone;
+    [SerializeField]
+    private PersistableObject[] _persistentObjects;
+
+    public static GameLevel Current { get; private set; }
+    public Vector3 SpawnPoint {
+        get { return _spawnZone.SpawnPoint; }
+    }
+
+    private void OnEnable() {
+        Current = this;
+        if (_persistentObjects == null) {
+            _persistentObjects = new PersistableObject[0];
+        }
+    }
+
+    public override void Save(GameDataWriter writer) {
+        writer.Write(_persistentObjects.Length);
+        for (int i = 0; i < _persistentObjects.Length; i++) {
+            _persistentObjects[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader) {
+        int savedCount = reader.ReadInt();
+        for (int i = 0; i < savedCount; i++) {
+            _persistentObjects[i].Load(reader);
+        }
+    }
+}
---
+++ [More_Game_State]/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotatingObject : PersistableObject
+{
+    [SerializeField]
+    private Vector3 _angularVelocity;
+
+    private void FixedUpdate() {
+        transform.Rotate(_angularVelocity * Time.deltaTime);
+    }
+}
