d5bcfbee5a044e6f3b28ae4be7d3b58f6d896272
Andrei <andreiazotov@gmail.com>
2019-04-02 09:42:29 +0300

    [CONFIG.SHAPES] init proj

---
+++ [Configuring_Shapes]/Assets/Scripts/PersistentStorage.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PersistentStorage : MonoBehaviour
+{
+    private string _savePath;
+
+    private void Awake() {
+        _savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    public void Save(PersistableObject o, int version) {
+        using (var writer = new BinaryWriter(File.Open(_savePath, FileMode.Create))) {
+            writer.Write(-version);
+            o.Save(new GameDataWriter(writer));
+        }
+    }
+
+    public void Load(PersistableObject o) {
+        var data = File.ReadAllBytes(_savePath);
+        var reader = new BinaryReader(new MemoryStream(data));
+        o.Load(new GameDataReader(reader, -reader.ReadInt32()));
+    }
+}
---
+++ [Configuring_Shapes]/Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class SpawnZone : PersistableObject
+{
+    public abstract Vector3 SpawnPoint { get; }
+}
